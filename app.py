07a52ad026d8091ff08cd3f8f800444ad5d8d724
Javier Padilla <jpadilla@lyntia.com>
2024-10-06 23:45:19 +0200

    MyApiParaGlobant

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, request, jsonify
+from utils.sqlConnection import insert_to_sql,create_to_sql
+import pandas as pd
+import os
+
+app = Flask(__name__)
+
+@app.route('/holaMundo', methods=['GET'])
+def prueba():
+    return 'holaMundo 12345'
+
+@app.route('/upload-csv', methods=['POST'])
+def upload_csv():
+    # Verifica si se recibe algun archivo.
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'}), 400
+    
+    file = request.files['file']
+
+    # Verifica si el archivo tiene un nombre, 
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
+    
+    fileName = file.filename
+
+    # Verifica si el archivo es un CSV
+    if not file.filename.endswith('.csv'):
+        return jsonify({'error': 'File type not allowed. Only CSV files are accepted.'}), 400
+
+    #Verificar que el nombre del fichero esta entre los tres que puede leer
+    if (file.filename not in {'jobs.csv','hired_employees.csv','departments.csv' }):
+        return jsonify({'error': 'Not a valid file Name'}), 400
+
+    targetTable = fileName.replace(".csv","")
+
+    # Lee el archivo CSV utilizando pandas
+    try:
+        df = pd.read_csv(file, header=None)
+        if (targetTable =='jobs'):
+            df.columns = ['id', 'job']
+        if (targetTable =='departments'):
+            df.columns = ['id', 'department']
+        if (targetTable =='hired_employees'):
+            df.columns = ['id', 'name','datetime','department_id','job_id']
+        create_to_sql(df, targetTable)
+        insert_to_sql(df, targetTable)
+        # Mostrar en pantalla contenido fichero.
+        #return jsonify({'message': 'CSV file received and loaded successfully', 'data': create_to_sql(df, targetTable)}), 200
+        return jsonify({'message': 'CSV file received and loaded in table successfully', 'data': df.to_dict(orient='records')}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
---
+++ utils/sqlConnection.py
@@ -0,0 +1,112 @@
+
+# sqlConnect.py
+
+import pandas as pd
+from sqlalchemy import create_engine
+import pyodbc
+
+
+def obteinConnStr():
+    server = 'ufinetpowerbisql.database.windows.net'
+    database = 'PowerBI-Desa'
+    username = 'adminpowerbi'
+    password = 'ZSzdqiuQ1YamBqhg4bX5'
+     # Create a connection string
+    conn_str = (
+        f"DRIVER={{ODBC Driver 17 for SQL Server}};"
+        f"SERVER={server};"
+        f"DATABASE={database};"
+        f"UID={username};"
+        f"PWD={password}"
+    )
+    return conn_str
+
+def create_to_sql(df, targetTable):
+
+    conn_str = obteinConnStr()
+    typeConversion = {
+        'int64': 'INTEGER',
+        'object': 'VARCHAR(200)',
+        'float64': 'INTEGER'
+    }
+    # Connect to SQL server
+    conn = pyodbc.connect(conn_str)
+    cursor = conn.cursor()
+    
+    # Reemplazar NaN con valores adecuados
+    df = df.fillna('')
+
+    #Crear la tabla si no existe
+    columns = ", ".join([f"{col} {typeConversion[str(df[col].dtype)] }" for col in df.columns])
+    query = f"""
+    IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='{targetTable}' AND xtype='U')
+    CREATE TABLE {targetTable} (
+        {columns}
+    )
+    """
+    
+    print(query)  # Imprimir la consulta y los valores
+    cursor.execute(query)
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+    print("Tabla creada exitosamente")
+
+def insert_to_sql(df, targetTable):
+ 
+    conn_str = obteinConnStr()
+    # Connect to SQL server
+    conn = pyodbc.connect(conn_str)
+    cursor = conn.cursor()
+
+     # Reemplazar NaN con valores adecuados
+    df = df.fillna('')
+
+     # Insertar datos del DataFrame en la tabla SQL
+    placeholders = ", ".join(["?" for _ in df.columns])
+
+    # Con un batchsize de 1000 registros
+    batch_size = 1000
+    batch = []
+
+    for index, row in df.iterrows():
+        batch.append(tuple(row))
+        if len(batch) == batch_size:
+            query = f"""
+            INSERT INTO {targetTable} ({", ".join(df.columns)}) 
+            VALUES ({placeholders})
+            """
+            #print(query)  # Imprimir la consulta y los valores
+            cursor.executemany(query, batch)
+            conn.commit()
+            batch = []
+    
+    # Insertar si quedan registros restantes
+    if batch:
+        query = f"""
+        INSERT INTO {targetTable} ({", ".join(df.columns)}) 
+        VALUES ({placeholders})
+        """
+        #print(query)  # Imprimir la consulta y los valores
+        cursor.executemany(query, batch)
+        conn.commit()
+        
+    conn.commit()
+    cursor.close()
+    conn.close()
+    print("Datos insertados exitosamente")
+
+def write_dataframe_to_sql(df, table_name):
+    # Crear la cadena de conexión
+    conn_str = (
+        f"mssql+pyodbc://{username}:{password}@{server}/{database}"
+        "?driver=ODBC+Driver+17+for+SQL+Server"
+    )
+    
+    # Crear el motor de conexión
+    engine = create_engine(conn_str)
+    
+    # Escribir el DataFrame en la base de datos SQL
+    df.to_sql(table_name, engine, if_exists='replace', index=False)
+    print("DataFrame escrito en la base de datos exitosamente")
